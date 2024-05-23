345f0d3e1f85f6a36b47a7d87fb5b5fb3d250352
Frank Carey <fcarey@gmail.com>
2024-05-23 15:01:58 -0400

    Slow time and make character fall by removing collider.

--- scenes/kill_zone.gd
+++ scenes/kill_zone.gd
@@ -4,8 +4,12 @@ extends Area2D
 
 func _on_body_entered(body):
 	print("you died")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
+	
 
 func _on_timer_timeout():
 	# Restarts the game.
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
