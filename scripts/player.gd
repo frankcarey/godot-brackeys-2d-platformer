5892c51e07fa4a724b82e584110ebdfba7bf0c36
Frank Carey <fcarey@gmail.com>
2024-05-25 00:12:49 -0400

    Set a custom data of 'material' for the surface of tiles and make slick ice tiles.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -200.0
 const ACCEL = 1000. # this accelerates to about full after 4 squares.
@@ -22,8 +21,6 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 enum State {
 	DEAD,
 	IDLE,
-	JUMP,
-	DROP
 }
 var state: State
 
@@ -45,7 +42,7 @@ func kill():
 	$CollisionShape2D.queue_free()
 	restart_timer.start()
 
-func _process(delta):	
+func handle_input():	
 	if Input.is_action_just_pressed("jump"):
 		if Input.is_action_pressed("ui_down"):
 			drop()
@@ -55,41 +52,37 @@ func _process(delta):
 
 func jump():
 	if is_on_floor():
-		state = State.JUMP
 		audio_stream_player_2d.stream = jump_sound
 		audio_stream_player_2d.volume_db = -8 # lower volume
 		audio_stream_player_2d.play()
+		velocity.y = JUMP_VELOCITY + (abs(velocity.x) * JUMP_BOOST_MULT)
 
 func drop():
 	if is_on_floor():
-		state = State.DROP
 		audio_stream_player_2d.stream = drop_sound
 		audio_stream_player_2d.volume_db = -8 # lower volume
 		audio_stream_player_2d.play()
+		position.y += 1 # causes player to drop through one way colliders.
+
+func get_floor_material():
+	var floor = get_last_slide_collision()
+	if is_on_floor() and floor:
+		var hit_object = floor.get_collider()
+		if hit_object is TileMap:
+			var rid = floor.get_collider_rid()
+			var coord: Vector2 = hit_object.get_coords_for_body_rid(rid)
+			var layer: int = hit_object.get_layer_for_body_rid(rid)
+			var tiledata: TileData = hit_object.get_cell_tile_data(layer, coord)
+			var mat = tiledata.get_custom_data("material")
+			return mat
+
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	match state:
-		State.DROP:
-			position.y += 1
-			state = State.IDLE
-		State.JUMP:
-			velocity.y = JUMP_VELOCITY + (abs(velocity.x) * JUMP_BOOST_MULT)
-			state = State.IDLE
-		State.DEAD:
-			velocity.y += gravity * delta * 3
 	
+	handle_input()
 	
-	# HANDLE MOVEMENT INPUT (LEFT/RIGHT)
 	var direction = Input.get_axis("move_left", "move_right")
-	if direction > 0:
-		animated_sprite_2d.flip_h = false
-	elif direction < 0:
-		animated_sprite_2d.flip_h = true
-	
-	# Play animations
+	# Add the gravity.
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite_2d.play("idle")
@@ -97,15 +90,33 @@ func _physics_process(delta):
 			animated_sprite_2d.play("run")
 	else:
 		animated_sprite_2d.play("jump")
+		match state:
+			State.DEAD:
+				velocity.y += gravity * delta * 3
+			_:
+				velocity.y += gravity * delta
 	
+	# HANDLE MOVEMENT INPUT (LEFT/RIGHT)
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
+	
+	var tile_material = get_floor_material()
+	print(tile_material)
+	var stickiness = 1
+	if tile_material == "ice":
+		stickiness = 0
+
+	# direction is between -1 and +1 based on the controller value.
 	if direction:
-		var speed_limiter = abs(direction)**2 + SPEED
+		var speed_limiter = abs(direction)**4 + SPEED
 		velocity.x = clampf(velocity.x + (direction * ACCEL * delta), -speed_limiter, speed_limiter)
 	else:
-		velocity.x = move_toward(velocity.x, 0, DRAG * delta)
+		velocity.x = move_toward(velocity.x, 0, DRAG * delta * stickiness)
 
 	move_and_slide()
-	
 
 func _on_timer_timeout():
 	# Restarts the game.
