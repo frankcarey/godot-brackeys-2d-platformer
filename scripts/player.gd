d01272e6a78a01e4e2c521a0da49646d9992402c
Frank Carey <fcarey@gmail.com>
2024-05-24 20:51:53 -0400

    Add simple run acceleration and jump boost based on horizontal speed.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,9 @@ extends CharacterBody2D
 
 
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -200.0
+const ACCEL = 1000. # this accelerates to about full after 4 squares.
+const JUMP_BOOST_MULT = -1. # this approximately doubles the jump height with 130 speed.
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -20,7 +22,7 @@ func _physics_process(delta):
 		if Input.is_action_pressed("ui_down") and is_on_floor():
 			position.y += 1
 		else:
-			velocity.y = JUMP_VELOCITY
+			velocity.y = JUMP_VELOCITY + (abs(velocity.x) * JUMP_BOOST_MULT)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -39,11 +41,9 @@ func _physics_process(delta):
 			animated_sprite_2d.play("run")
 	else:
 		animated_sprite_2d.play("jump")
-		
-	
 	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = clampf(velocity.x + (direction * ACCEL * delta), -SPEED, SPEED)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
