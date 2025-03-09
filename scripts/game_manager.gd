7864712b52c2f7b18222afb013262ba1ad0dd1a7
Frank Carey <fcarey@gmail.com>
2025-03-08 22:41:44 -0500

    Fix level_time calculation and use accept_dialogues that pause the game.

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,6 +6,7 @@ extends Node
 @export var timer_label: Label
 @export var score_label: Label
 @export var death_label: Label
+@export var accept_dialog: AcceptDialog
 
 @export var coins_gathered: Array[String] = []
 
@@ -15,6 +16,19 @@ var init = false
 var score = 0
 var total_coins = 0
 var deaths = 0
+var callback: Callable
+var is_paused = false
+var start_time = Time.get_ticks_msec()
+
+func pause(do_pause: bool):
+    # Pause the game
+    get_tree().paused = do_pause
+    is_paused = do_pause
+
+func get_level_time() -> float:
+    # Get the time since the level started
+    return (Time.get_ticks_msec() - start_time) / 1000.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,12 +39,49 @@ func _ready():
     else:
         player.connect("player_died", _on_player_died)
 
+    #Show the start dialog
+    show_dialog("Welcome to the game!",
+        "Press OK to start the game.",
+        "OK",
+        _on_accept_dialog_start
+    )
+
+func _on_accept_dialog_start():
+    restart_from_last_checkpoint()
+    start_time = Time.get_ticks_msec()
+
+func show_dialog(title: String, message: String, button_text: String, callback: Callable):
+    # Show the dialog
+    pause(true)
+    accept_dialog.popup_centered()
+    accept_dialog.confirmed.connect(_on_accept_dialog_confirmed)
+    accept_dialog.get_ok_button().text = button_text
+    accept_dialog.dialog_text = message
+    accept_dialog.title = title
+    accept_dialog.process_mode = PROCESS_MODE_WHEN_PAUSED
+    self.callback = callback
+
+func _on_accept_dialog_confirmed():
+    # Hide the dialog
+    accept_dialog.hide()
+    # Disconnect the signal to avoid multiple connections
+    accept_dialog.confirmed.disconnect(_on_accept_dialog_confirmed)
+    pause(false)
+    accept_dialog.process_mode = PROCESS_MODE_INHERIT
+    if callback:
+        # Call the callback function if provided
+        callback.call()
+    else:
+        print("Dialog callback is null")
+
 func _process(delta):
     if not init:
         restart_from_last_checkpoint()
         init = true
+    if is_paused:
+        return
     # Update the timer label in format "Time: 0.000s"
-    timer_label.text = "Time: " + "%8.1f" % (Time.get_ticks_msec() / 1000.0) + "s"
+    timer_label.text = "Time: " + "%8.1f" % ((Time.get_ticks_msec() - start_time) / 1000.0) + "s"
 
 
 func update_score():
@@ -57,6 +108,7 @@ func get_coins():
             continue
         print(coin.name + " not in coins_gathered")
         coin.coin_collected.connect(_on_coin_collected)
+    # total_coins = 1 # For testing the end screen, set total coins to 1
     update_score()
 
 
@@ -76,16 +128,29 @@ func gather_coin(coin: Node2D):
     # Add a point to the score
     add_point()
 
+    if score == total_coins:
+        # Show the dialog
+
+        show_dialog("All coins collected!", "All coins collected!\n\n" + \
+            str(score) + "/" + str(total_coins) + " coins collected.\n\n" + \
+            "Completed Time: " + "%8.1f" % get_level_time() + "s\n\n" + \
+            "Deaths: " + str(deaths) + "\n\n" + \
+            "Press Restart Level to continue.", "Restart Level",
+        _on_accept_dialog_endscreen)
+    else:
+        # Update the score label
+        update_score()
+
+func _on_accept_dialog_endscreen():
+    # Load the next level
+    get_tree().reload_current_scene()
+
 func _on_player_died(_p: Node2D):
     print("Player died")
     deaths += 1
     death_label.text = "Deaths: " + str(deaths)
     # Restart the game
     restart_from_last_checkpoint()
-
-func _on_checkbox_button_toggled(pressed: bool):
-    pass
-
 
 func restart_from_last_checkpoint():
     print("Restarting from last checkpoint")
